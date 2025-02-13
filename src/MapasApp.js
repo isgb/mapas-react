0c92ae1b0dc9db02c3b2d6d521fe6baac4dd350b
isaias <isgb_codigo@hotmail.com>
2025-02-13 00:56:15 -0600

    avances

---
+++ src/MapasApp.js
@@ -0,0 +1,51 @@
+import React, { useState, useEffect } from "react";
+import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
+import "leaflet/dist/leaflet.css";
+import L from "leaflet";
+
+export const MapasApp = () => {
+  const [position, setPosition] = useState(null);
+
+  useEffect(() => {
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(
+        (position) => {
+          const { latitude, longitude } = position.coords;
+          setPosition([latitude, longitude]);
+        },
+        (error) => {
+          console.error("Error getting geolocation:", error);
+        }
+      );
+    } else {
+      console.error("Geolocation is not supported by this browser.");
+    }
+  }, []);
+
+  const defaultIcon = L.icon({
+    iconUrl: 'https://unpkg.com/leaflet@1.7.1/dist/images/marker-icon.png',
+    iconSize: [25, 41],
+    iconAnchor: [12, 41],
+    popupAnchor: [1, -34],
+    shadowUrl: 'https://unpkg.com/leaflet@1.7.1/dist/images/marker-shadow.png',
+    shadowSize: [41, 41]
+  });
+
+  return (
+    <div>
+      {position ? (
+        <MapContainer center={position} zoom={13} style={{ height: "100vh", width: "100%" }}>
+          <TileLayer
+            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+            attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+          />
+          <Marker position={position} icon={defaultIcon}>
+            <Popup>Tu ubicación actual</Popup>
+          </Marker>
+        </MapContainer>
+      ) : (
+        <p>Obteniendo tu ubicación...</p>
+      )}
+    </div>
+  );
+};
